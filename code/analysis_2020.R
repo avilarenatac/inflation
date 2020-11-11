902ecee6ddec7d24fdf48951f287c09e08ef4fd7
avilarenatac <avilarenatac@gmail.com>
2020-11-11 16:30:07 -0300

    Minor fixes in 2020 script

--- code/analysis_2020.R
+++ code/analysis_2020.R
@@ -2,6 +2,12 @@
 #' @description Import Brazilian CPI data and plot graphs
 #' @author Renata Avila
 
+
+# The data used in this script is retrieved from the IBGE - the Brazilian NSO - using the 'sidrar' package
+# Function 'get_sidra' allows R to connect with IBGE's API
+# Parameters passed retrieve the series of interest
+
+
 # Packages ----------------------------------------------------------------
 library(sidrar)
 library(tidyverse)
@@ -10,23 +16,17 @@ library(RColorBrewer)
 library(viridis)
 
 
-# Paths -------------------------------------------------------------------
-root    <- "/Users/renata/Documents/inflation"
-OUTDIR  <- file.path(root, "output") 
 
-
-
-# Import IPCA (Brazilian inflation) disaggregated data ----------------------
+# Import IPCA (Brazilian CPI) disaggregated data ----------------------
 tab_ipca_2020 <- sidrar::get_sidra(api = "/t/7060/n1/all/v/all/p/last%2012/c315/all/d/v63%202,v66%204")
 
 
 
-# Import CPI aggregates: monthly, yearly, last 12 months --------------------
+# Import CPI aggregates: monthly and aggregated over last 12 months --------------------
 tab_ipca <- sidrar::get_sidra(api = "/t/1737/n1/all/v/all/p/last%2013/d/v63%202")
 
 
-
-# Calculate Incidences of each group to monthly index ------------------
+# Calculate the incidence of each group in the aggregate monthly index ------------
 incidence_table <- tab_ipca_2020 %>%
                     filter(Variável %in% c("IPCA - Variação mensal", "IPCA - Peso mensal"),
                            !stringr::str_detect(`Geral, grupo, subgrupo, item e subitem`, "\\d{2}")) %>%
@@ -55,18 +55,15 @@ ggplot(incidence_table %>%
   # Draw line for aggregate result
   geom_line(data = subset(incidence_table, Group == "Índice geral"),
             aes(y = Incidence)) +
-  
-  # Set color and labels 
-  # Translate labels to English
   scale_fill_brewer(palette = "PuBu", 
+                    # Translate Portuguese labels to English
                     labels = c("Food and Beverages", "Housing", "Residency",
                                "Clothing and Apparel", "Transportation",
                                "Healthcare", "Personal expenditures",
                                "Education", "Communication")) +
-  # Theme and titles
   theme_minimal() +
   labs(x = "", y = "Incidence", 
-       title = "IPCA (monthly) - Incidences by group")
+       title = "IPCA (monthly) - Incidence by group")
 
 ggsave(paste0("./output/", "cpi_brazil_groups.png"), width = 12, height = 8, units = "in")
 
@@ -79,49 +76,35 @@ ggsave(paste0("./output/", "cpi_brazil_groups.png"), width = 12, height = 8, uni
 
 
 
-# Create table with inflation aggregates -----------------------------------------
+# Table with inflation aggregates for 2020 ----------------------------------
 tab_agg <- tab_ipca %>%
 
   # Select monthly, 12 months and year aggregates
-  dplyr::filter(`Variável` == "IPCA - Variação mensal" |
+  filter(`Variável` == "IPCA - Variação mensal" |
                 `Variável` == "IPCA - Variação acumulada em 12 meses" |
                 `Variável` ==  "IPCA - Variação acumulada no ano",
                   ) %>%
-  # Format dates
-  dplyr::mutate(Date = parse_date(`Mês (Código)`, format = "%Y%m")) %>%
-  
-  # Filter for 2020 values
+  mutate(Date = parse_date(`Mês (Código)`, format = "%Y%m")) %>%
   filter(lubridate::year(Date) == 2020) %>%
-
-  # Select relevant variables
-  dplyr::select(Date, 
-                variable = Variável,
-                value    = Valor) 
+  select(Date, 
+         variable = Variável,
+         value    = Valor) 
 
     
 # Plot --------------------------------------------------------------------
 ggplot(tab_agg, aes(x = Date, y = value)) +
-  
-  # Columns for each variable
   geom_col(aes(fill = variable)) +
-  
-  # Legend and annotation
   scale_fill_brewer(palette = "Blues",
                     labels = c("Last 12 months", "Year aggregate", "Month")) +
   scale_x_date(date_breaks = "1 month", date_labels = "%b/%Y") +
   geom_text(aes(label = value), position = position_stack(vjust = 0.5),
             check_overlap = TRUE) +
-  # geom_text(aes(label = value), vjust = 1.5, check_overlap = TRUE) +
-  #  geom_text(aes(label = value), position = position_dodge(0.9)) +
 
-
-  # Theme and title
     labs(title = "IPCA - Brazilian CPI",
        x = "", y = "", alpha = "") +
     theme_minimal() +
     theme(legend.position = "bottom",
-        legend.title = element_blank()) 
-
+          legend.title = element_blank())
 
    
 ggsave(paste0("./output/", "cpi_brazil_aggregates.png"), width = 12, height = 8, units = "in")
